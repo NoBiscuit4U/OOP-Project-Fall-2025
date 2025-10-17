44a6bdebcb5cf760657c9bf1bd2f5d7524d979aa
BoetigJ <jacobboetig@gmail.com>
2025-10-17 13:47:07 -0500

    menu1

--- menu.py
+++ menu.py
@@ -3,25 +3,14 @@ import pickle
 
 print("Welcome to the _____ library management system.")
 
-list_of_options = { "Option 1":"1",
-                    "Option 2":"2",
-                    "Option 3":"3",
-                    "Option 4":"4",
-                    "Option 5":"5",
-                    "Option 6":"6",
-                    "Option 7":"7",
-                    "Option 8":"8",
-                    "Option 9":"9",
-
-
-                    "To Quit": "quit"
-                    }
+list_of_options = ["display menu, quit"]
 
 while True:
     user_choice = input("What would you like to do? ")
 
-    if user_choice == "1":
-        break
+    if user_choice == "display menu":
+        for option in list_of_options:
+            print(option)
 
     elif user_choice == "2":
         break
