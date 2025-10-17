7ced074da4970bbcb3abf36f9ed1da99d573ada8
BoetigJ <jacobboetig@gmail.com>
2025-10-17 13:41:15 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,63 @@
+#import user, editor, and book classes
+import pickle
+
+print("Welcome to the _____ library management system.")
+
+list_of_options = { "Option 1":"1",
+                    "Option 2":"2",
+                    "Option 3":"3",
+                    "Option 4":"4",
+                    "Option 5":"5",
+                    "Option 6":"6",
+                    "Option 7":"7",
+                    "Option 8":"8",
+                    "Option 9":"9",
+
+
+                    "To Quit": "quit"
+                    }
+
+while True:
+    user_choice = input("What would you like to do? ")
+
+    if user_choice == "1":
+        break
+
+    elif user_choice == "2":
+        break
+
+    elif user_choice == "3":
+        break
+
+    elif user_choice == "4":
+        break
+
+    elif user_choice == "5":
+        break
+
+    elif user_choice == "6":
+        break
+
+    elif user_choice == "7":
+        break
+
+    elif user_choice == "8":
+        break
+
+    elif user_choice == "9":
+        break
+
+
+
+
+
+
+
+
+
+
+    elif user_choice == "quit":
+        break
+
+    else:
+        print("Sorry, that is not a valid option. Please try again.")
