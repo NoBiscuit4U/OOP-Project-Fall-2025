eb82eebd3088ebbf99040481c79152c923f7ce45
BoetigJ <jacobboetig@gmail.com>
2025-11-19 13:30:47 -0600

    menu1.1

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import UserManager
+from User import User
 
 um=UserManager.UserManager()
 
@@ -27,3 +28,15 @@ else:
     with st.container(horizontal_alignment="center"):
         st.button("Access Users", key="users_access")
         st.button("Access Books", key="books_access")
+
+
+        if st.session_state.users_access:
+            st.button("Add User", key="add_user")
+            st.text_input("ID", key="user_input_id")
+            st.text_input("Name", key="user_input_name")
+            st.text_input("Password", key="user_input_password")
+            st.text_input("Credentials", key="user_input_creds")
+            st.text_input("Email", key="user_input_email")
+            user = User(st.session_state.add_user)
+
+
