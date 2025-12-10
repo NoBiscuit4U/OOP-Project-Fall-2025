9ec295d3d51fc4534445ffe6ccb4e9cbad8890fc
BoetigJ <jacobboetig@gmail.com>
2025-12-10 13:17:09 -0600

    menu1.1

--- book_checkout.py
+++ book_checkout.py
@@ -8,11 +8,10 @@ with st.container(horizontal_alignment="center"):
     st.text_input("Search", key="book_search_input")
     st.selectbox("Search Criteria", options=["ID", "Author", "Title"], key="book_search")
 
+    st.button("Search", key="return_book_search")
 
-
-
-    confusion_matrix = pd.DataFrame(
-        st.session_state.bm.get_books_info(),
-        columns=st.session_state.bm.get_keys()
-        )
-    st.dataframe(confusion_matrix, height="auto")
+    if st.session_state.return_book_search:
+        if st.session_state.book_search == ID:
+            for book in pd.DataFrame():
+                #if the book's ID is equal to a book ID, show the book
+                # if not say this book is not in the library.
